b093a6c2ab42595ede76c951f233056c3ec05f1c
omkarrathod123 <omkarrathod0705@gmail.com>
2026-03-04 19:26:37 +0530

    Adding Area Caluclator

---
+++ Pages/AreaCalculator.razor.cs
@@ -0,0 +1,22 @@
+﻿namespace Length_Calculator.Pages
+{
+    public partial class AreaCalculator
+    {
+        public string statement {  get; set; }
+        public double length { get; set; }
+        public double width { get; set; }
+        public double area { get; set; }
+        public void CalArea()
+        {
+            if (length == 0 && width == 0)
+            {
+                statement = "length and width can not be 0 ( Zero )";
+            }
+            else
+            {
+                area = length * width;
+                statement = "Area of Rectangle: " + area;
+            }
+        }
+    }
+}
